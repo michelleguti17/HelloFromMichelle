2e5e8213fc210a58fb9390c34b461018cac3883c
michelleguti17 <gut20014@byui.edu>
2021-10-08 11:45:46 -0500

    This is the reverse change.

--- src/Gutierrez/HelloFromMichelle.java
+++ src/Gutierrez/HelloFromMichelle.java
@@ -2,6 +2,6 @@ package Gutierrez;
 
 public class HelloFromMichelle {
     public static void  main (String args[]){
-        System.out.println("Hello, my name is Miche");
+        System.out.println("Hello, my name is Michelle");
     }
 }
