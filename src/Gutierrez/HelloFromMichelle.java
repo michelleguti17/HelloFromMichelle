b3f4fd09a1ddf2d0d61ba5809830cbb1bb4d4e5b
sanele23 <sanele23@live.com>
2021-10-09 13:32:04 +0200

    Updated response

--- src/Gutierrez/HelloFromMichelle.java
+++ src/Gutierrez/HelloFromMichelle.java
@@ -2,7 +2,8 @@ package Gutierrez;
 
 public class HelloFromMichelle {
     public static void  main (String args[]){
+
         System.out.println("Hello, my name is Michelle");
-        System.out.println("Hello Michelle, Sanele here);
+        System.out.println("Hello Michelle, my name is Sanele");
     }
 }
